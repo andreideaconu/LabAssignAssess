86d982c20f905e2a26d2d4ebde67c1a381cd2e24
DobosP <pdobos@yahoo.om>
2020-04-11 18:48:19 +0300

    Correct validation Errors

--- src/main/java/LabAssignAssessMV/validation/TemaValidator.java
+++ src/main/java/LabAssignAssessMV/validation/TemaValidator.java
@@ -14,7 +14,7 @@ public class TemaValidator implements Validator<Tema> {
         if(entity.getID().equals("") || entity.getID() == null) {
             throw new ValidationException("Numar tema invalid!");
         }
-        if(entity.getDescriere().equals("")){
+        if(entity.getDescriere().equals("") || entity.getDescriere() == null){
             throw new ValidationException("Descriere invalida!");
         }
         if(entity.getDeadline() < 1 || entity.getDeadline() > 14) {
@@ -22,6 +22,9 @@ public class TemaValidator implements Validator<Tema> {
         }
         if(entity.getPrimire() < 1 || entity.getPrimire() > 14) {
             throw new ValidationException("Saptamana primirii trebuie sa fie intre 1-14.");
+        }
+        if(entity.getPrimire() > entity.getDeadline()) {
+            throw new ValidationException("Primire nu poate fii mai mare decat deadline");
         }
     }
 }
